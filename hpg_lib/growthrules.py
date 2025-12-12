257e0b58ead77856c7f75b641cbe0ee0b3983af0
Josh Swanson <jpswanson3.14@gmail.com>
2025-12-12 03:02:48 -0500

    Partially corrected growth rules. They give the right connectivity, but trips and faces are broken

--- hpg_lib/growthrules.py
+++ hpg_lib/growthrules.py
@@ -21,6 +21,7 @@ from itertools import chain
 from random import random
 from .hourglassplabicgraph import HourglassPlabicGraph
 from .prom import to_lattice_word
+from .idgenerator import ID
 
 def occurrences(big, small):
     '''Consecutive occurrences.'''
@@ -70,40 +71,50 @@ def get_nonelliptic_web(T):
     for i in range(n):
         v = danglers[i]
         hhs = list(v)
-        base = hhs[0] if hhs[0].v_to() == danglers[(i-1)%n] else hhs[1]
+        base = hhs[0] if hhs[0].v_to() == danglers[(i+1)%n] else hhs[1]
+        base = base.twin()
         base_hhs.append(base)
 
+#    print("base_hhs's", [hh.id for hh in base_hhs])
     while True:
         for Lp, i, s in chain(*(move(L) for move in _nonelliptic_local_moves)):
             v1 = danglers[i]
             v2 = danglers[i+1]
+#            print("danglers", v1.id, v2.id)
 
             if s=="Y":
-                v_new = G.create_vertex(random(), random(), False if Lp[i]<0 else True)
-                hh1 = G.create_hourglass(v1, v_new, 1, base_hhs[i], None)
-                hh2 = G.create_hourglass(v2, v_new, 1, hh1, None)
+#                print("...Y move")
+                v_new = G.create_vertex(ID.get_new_id("k"), random(), random(), False if Lp[i]<0 else True)
+#                print("...v_new", v_new.id)
+#                print("...v1 hh's before", v1.id, [hh.id for hh in v1])
+#                print("...v2 hh's before", v2.id, [hh.id for hh in v2])
+                hh1 = G.create_hourglass(v_new, v1, 1, base_hhs[i], None)
+                hh2 = G.create_hourglass(v_new, v2, 1, hh1, None)
+#                print("...v1 hh's after", v1.id, [hh.id for hh in v1])
+#                print("...v2 hh's after", v2.id, [hh.id for hh in v2])
+#                print("...v_new hh's after", v_new.id, [hh.id for hh in v_new])
                 danglers[i] = v_new
-                base_hhs[i] = hh1
+                base_hhs[i] = hh2
                 del danglers[i+1]
                 del base_hhs[i+1]
                 
             elif s=="H":
-                v1_new = G.create_vertex(random(), random(), False if Lp[i]<0 else True)
-                v2_new = G.create_vertex(random(), random(), False if Lp[i+1]<0 else True)
-                hh1 = G.create_hourglass(v1, v1_new, 1, base_hhs[i], None)
-                hh2 = G.create_hourglass(v2, v2_new, 1, base_hhs[i+1], None)
-                hhx = G.create_hourglass(v1_new, v2_new, 1, None, None)
+                v1_new = G.create_vertex(ID.get_new_id("k"), random(), random(), False if Lp[i]<0 else True)
+                v2_new = G.create_vertex(ID.get_new_id("k"), random(), random(), False if Lp[i+1]<0 else True)
+                hh1 = G.create_hourglass(v1_new, v1, 1, base_hhs[i], None)
+                hh2 = G.create_hourglass(v2_new, v2, 1, base_hhs[i+1], None)
+                hhx = G.create_hourglass(v2_new, v1_new, 1, None, None)
                 danglers[i] = v1_new
                 danglers[i+1] = v2_new
-                base_hhs[i] = hh1
-                base_hhs[i+1] = hhx
+                base_hhs[i] = hhx
+                base_hhs[i+1] = hh2
             
             elif s=="cup":
-                G.create_hourglass(v1, v2, 1, base_hhs[i], base_hhs[i+1])
+                G.create_hourglass(v1, v2, 1, base_hhs[i], base_hhs[i+1].twin())
                 del danglers[i:i+2]
                 del base_hhs[i:i+2]
                 
-            print(Lp, i, s)
+#            print(Lp, i, s)
             
             L = Lp
             break
